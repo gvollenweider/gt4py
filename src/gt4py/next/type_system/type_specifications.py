33228ecba83fc260d90deb91e6db0a50fbb50d46
Hannes Vogt <hannes@havogt.de>
2024-11-25 14:22:24 +0100

    feat[next]: remove NeighborTableOffsetProvider, use gtx.as_connectivity (#1729)
    User-facing change: use `gtx.as_connectivity` to create a
    connectivity/neighbor table instead of `NeighborTableOffsetProvider`
    which is deprecated (and the backward-compatible mechanism broken for
    some use-cases).
    
    The internal concepts of `Connectivity` and `NeighborTable` are updated.
    `ConnectivityType` is introduced which contains the compile-time info of
    a `Connectivity`. See ADR 19.
    Additionally, the compile-time info is used (instead of the run-time
    connectivities) in many places of the toolchain when possible.
    # Conflicts:
    #	src/gt4py/next/program_processors/runners/dace_iterator/itir_to_tasklet.py
    #	src/gt4py/next/type_system/type_specifications.py
    #	tests/next_tests/integration_tests/multi_feature_tests/ffront_tests/test_ffront_fvm_nabla.py
    #	tests/next_tests/unit_tests/iterator_tests/test_runtime_domain.py

--- src/gt4py/next/type_system/type_specifications.py
+++ src/gt4py/next/type_system/type_specifications.py
@@ -6,13 +6,21 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
+from dataclasses import dataclass
 from typing import Iterator, Optional, Sequence, Union
 
-from gt4py.eve import datamodels as eve_datamodels, type_definitions as eve_types
-from gt4py.next import common
+from gt4py.eve.type_definitions import IntEnum
+from gt4py.eve.utils import content_hash
+from gt4py.next import common as func_common
 
 
-class TypeSpec(eve_datamodels.DataModel, kw_only=False, frozen=True): ...  # type: ignore[call-arg]
+@dataclass(frozen=True)
+class TypeSpec:
+    def __hash__(self) -> int:
+        return hash(content_hash(self))
+
+    def __init_subclass__(cls) -> None:
+        cls.__hash__ = TypeSpec.__hash__  # type: ignore[method-assign]
 
 
 class DataType(TypeSpec):
@@ -32,12 +40,14 @@ class CallableType:
     """
 
 
+@dataclass(frozen=True)
 class DeferredType(TypeSpec):
     """Dummy used to represent a type not yet inferred."""
 
     constraint: Optional[type[TypeSpec] | tuple[type[TypeSpec], ...]]
 
 
+@dataclass(frozen=True)
 class VoidType(TypeSpec):
     """
     Return type of a function without return values.
@@ -46,34 +56,31 @@ class VoidType(TypeSpec):
     """
 
 
+@dataclass(frozen=True)
 class DimensionType(TypeSpec):
-    dim: common.Dimension
+    dim: func_common.Dimension
 
 
+@dataclass(frozen=True)
 class OffsetType(TypeSpec):
     # TODO(havogt): replace by ConnectivityType
-    source: common.Dimension
-    target: tuple[common.Dimension] | tuple[common.Dimension, common.Dimension]
+    source: func_common.Dimension
+    target: tuple[func_common.Dimension] | tuple[func_common.Dimension, func_common.Dimension]
 
     def __str__(self) -> str:
         return f"Offset[{self.source}, {self.target}]"
 
 
-class ScalarKind(eve_types.IntEnum):
+class ScalarKind(IntEnum):
     BOOL = 1
-    INT8 = 2
-    UINT8 = 3
-    INT16 = 4
-    UINT16 = 5
-    INT32 = 6
-    UINT32 = 7
-    INT64 = 8
-    UINT64 = 9
-    FLOAT32 = 10
-    FLOAT64 = 11
-    STRING = 12
+    INT32 = 32
+    INT64 = 64
+    FLOAT32 = 1032
+    FLOAT64 = 1064
+    STRING = 3001
 
 
+@dataclass(frozen=True)
 class ScalarType(DataType):
     kind: ScalarKind
     shape: Optional[list[int]] = None
@@ -85,43 +92,31 @@ class ScalarType(DataType):
         return f"{kind_str}{self.shape}"
 
 
-class ListType(DataType):
-    """Represents a neighbor list in the ITIR representation.
-
-    Note: not used in the frontend. The concept is represented as Field with local Dimension.
-    """
-
-    element_type: DataType
-    # TODO(havogt): the `offset_type` is not yet used in type_inference,
-    # it is meant to describe the neighborhood (via the local dimension)
-    offset_type: Optional[common.Dimension] = None
-
-
-class FieldType(DataType, CallableType):
-    dims: list[common.Dimension]
-    dtype: ScalarType | ListType
-
-    def __str__(self) -> str:
-        dims = "..." if self.dims is Ellipsis else f"[{', '.join(dim.value for dim in self.dims)}]"
-        return f"Field[{dims}, {self.dtype}]"
-
-
+@dataclass(frozen=True)
 class TupleType(DataType):
-    # TODO(tehrengruber): Remove `DeferredType` again. This was erroneously
-    #  introduced before we checked the annotations at runtime. All attributes of
-    #  a type that are types themselves must be concrete.
-    types: list[DataType | DimensionType | DeferredType]
+    types: list[DataType]
 
     def __str__(self) -> str:
         return f"tuple[{', '.join(map(str, self.types))}]"
 
-    def __iter__(self) -> Iterator[DataType | DimensionType | DeferredType]:
+    def __iter__(self) -> Iterator[DataType]:
         yield from self.types
 
     def __len__(self) -> int:
         return len(self.types)
 
 
+@dataclass(frozen=True)
+class FieldType(DataType, CallableType):
+    dims: list[func_common.Dimension]
+    dtype: ScalarType
+
+    def __str__(self) -> str:
+        dims = "..." if self.dims is Ellipsis else f"[{', '.join(dim.value for dim in self.dims)}]"
+        return f"Field[{dims}, {self.dtype}]"
+
+
+@dataclass(frozen=True)
 class FunctionType(TypeSpec, CallableType):
     pos_only_args: Sequence[TypeSpec]
     pos_or_kw_args: dict[str, TypeSpec]
--- tests/next_tests/integration_tests/multi_feature_tests/ffront_tests/test_ffront_fvm_nabla.py
+++ tests/next_tests/integration_tests/multi_feature_tests/ffront_tests/test_ffront_fvm_nabla.py
@@ -13,7 +13,6 @@ import pytest
 
 pytest.importorskip("atlas4py")
 
-import gt4py._core.definitions as core_defs
 from gt4py import next as gtx
 from gt4py.next import allocators, neighbor_sum
 from gt4py.next.iterator import atlas_utils
@@ -63,15 +62,12 @@ def pnabla(
     return compute_pnabla(pp, S_M[0], sign, vol), compute_pnabla(pp, S_M[1], sign, vol)
 
 
-@pytest.mark.requires_atlas
 def test_ffront_compute_zavgS(exec_alloc_descriptor):
-    # TODO(havogt): fix nabla setup to work with GPU
-    if exec_alloc_descriptor.allocator.device_type != core_defs.DeviceType.CPU:
-        pytest.skip("This test is only supported on CPU devices yet")
+    _, allocator = exec_alloc_descriptor.executor, exec_alloc_descriptor.allocator
 
-    setup = nabla_setup(allocator=exec_alloc_descriptor.allocator)
+    setup = nabla_setup(allocator=allocator)
 
-    zavgS = gtx.zeros({Edge: setup.edges_size}, allocator=exec_alloc_descriptor.allocator)
+    zavgS = gtx.zeros({Edge: setup.edges_size}, allocator=allocator)
 
     compute_zavgS.with_backend(
         None if exec_alloc_descriptor.executor is None else exec_alloc_descriptor
@@ -86,16 +82,13 @@ def test_ffront_compute_zavgS(exec_alloc_descriptor):
     assert_close(388241977.58389181, np.max(zavgS.asnumpy()))
 
 
-@pytest.mark.requires_atlas
 def test_ffront_nabla(exec_alloc_descriptor):
-    # TODO(havogt): fix nabla setup to work with GPU
-    if exec_alloc_descriptor.allocator.device_type != core_defs.DeviceType.CPU:
-        pytest.skip("This test is only supported on CPU devices yet")
+    _, allocator = exec_alloc_descriptor.executor, exec_alloc_descriptor.allocator
 
-    setup = nabla_setup(allocator=exec_alloc_descriptor.allocator)
+    setup = nabla_setup(allocator=allocator)
 
-    pnabla_MXX = gtx.zeros({Vertex: setup.nodes_size}, allocator=exec_alloc_descriptor.allocator)
-    pnabla_MYY = gtx.zeros({Vertex: setup.nodes_size}, allocator=exec_alloc_descriptor.allocator)
+    pnabla_MXX = gtx.zeros({Vertex: setup.nodes_size}, allocator=allocator)
+    pnabla_MYY = gtx.zeros({Vertex: setup.nodes_size}, allocator=allocator)
 
     pnabla.with_backend(None if exec_alloc_descriptor.executor is None else exec_alloc_descriptor)(
         setup.input_field,
