b43c33f79a0b0c688fa5762c0dc81fd04f8d8307
Hannes Vogt <hannes@havogt.de>
2024-12-06 11:52:17 +0100

    refactor[next]: remove all FencilDefinitions from tests (#1773)
    # Conflicts:
    #	tests/next_tests/unit_tests/iterator_tests/test_pretty_parser.py
    #	tests/next_tests/unit_tests/iterator_tests/test_pretty_printer.py

--- tests/next_tests/unit_tests/iterator_tests/test_pretty_parser.py
+++ tests/next_tests/unit_tests/iterator_tests/test_pretty_parser.py
@@ -6,7 +6,7 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from gt4py.next.iterator import ir, builtins
+from gt4py.next.iterator import ir
 from gt4py.next.iterator.ir_utils import ir_makers as im
 from gt4py.next.iterator.pretty_parser import pparse
 from gt4py.next.type_system import type_specifications as ts
@@ -111,7 +111,7 @@ def test_tuple_get():
     testee = "x[42]"
     expected = ir.FunCall(
         fun=ir.SymRef(id="tuple_get"),
-        args=[im.literal("42", builtins.INTEGER_INDEX_BUILTIN), ir.SymRef(id="x")],
+        args=[im.literal("42", ir.INTEGER_INDEX_BUILTIN), ir.SymRef(id="x")],
     )
     actual = pparse(testee)
     assert actual == expected
@@ -127,7 +127,7 @@ def test_make_tuple():
 
 
 def test_named_range_horizontal():
-    testee = "IDimₕ: [x, y["
+    testee = "IDimₕ: [x, y)"
     expected = ir.FunCall(
         fun=ir.SymRef(id="named_range"),
         args=[ir.AxisLiteral(value="IDim"), ir.SymRef(id="x"), ir.SymRef(id="y")],
@@ -137,7 +137,7 @@ def test_named_range_horizontal():
 
 
 def test_named_range_vertical():
-    testee = "IDimᵥ: [x, y["
+    testee = "IDimᵥ: [x, y)"
     expected = ir.FunCall(
         fun=ir.SymRef(id="named_range"),
         args=[
