120177bb96238a22358ac27701f262b1a4d79182
stubbiali <subbiali@phys.ethz.ch>
2025-05-23 13:49:03 +0200

    Add support for for-loops in dace.

--- src/gt4py/cartesian/gtc/gtcpp/gtcpp_codegen.py
+++ src/gt4py/cartesian/gtc/gtcpp/gtcpp_codegen.py
@@ -182,8 +182,6 @@ class GTCppCodegen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
                 NativeFunction.FLOAT64: "double",
                 NativeFunction.ERF: "std::erf",
                 NativeFunction.ERFC: "std::erfc",
-                NativeFunction.F32: "float",
-                NativeFunction.F64: "double",
             }[func]
         except KeyError as error:
             raise NotImplementedError(
